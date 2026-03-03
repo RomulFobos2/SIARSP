71ce01746b0fd58e1194cbe6b7bb6f5d2c5d3d15
RomulFobos2 <38048117+RomulFobos2@users.noreply.github.com>
2026-03-04 00:33:09 +0300

    Improve courier mobile UI and automate GPS location sending

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
