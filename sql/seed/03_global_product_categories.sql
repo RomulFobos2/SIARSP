3a9fdeb960ef875e33b84de364bfe84a5b0d140e
RomulFobos2 <38048117+RomulFobos2@users.noreply.github.com>
2026-03-15 04:07:56 +0300

    Fix DB cleanup for MySQL FK constraints using DELETE + AUTO_INCREMENT reset

---
+++ sql/seed/03_global_product_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_global_product_category (name)
+VALUES
+('Молочная продукция'),
+('Мясная продукция'),
+('Овощи и фрукты');
---
+++ sql/seed/12_equipment_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_equipment_type (name)
+VALUES
+('Погрузчик'),
+('Холодильная камера'),
+('Электронные весы');
